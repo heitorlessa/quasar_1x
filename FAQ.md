efa2bf2559cefa7f667ad992dcebe59497d75ccb
heitorlessa <lessa@amazon.co.uk>
2020-11-09 18:40:30 +0100

    fix: linter; stylus->sass

---
+++ FAQ.md
@@ -0,0 +1,5 @@
+**Q: What are boot files and how do I use them?**
+
+**Q: How do I include Amplify?**
+
+
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,74 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header>
+      <q-toolbar class="text-center">
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+
+        <q-toolbar-title class="brand">Flight App</q-toolbar-title>
+        <q-btn
+          flat
+          round
+          dense
+          size="lg"
+          icon="search"
+          :to="{ name: 'home' }"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" content-class="bg-grey-1">
+      <q-list no-border link inset-delimiter>
+        <q-item-label header class="text-grey-8"> Menu </q-item-label>
+        <q-item :to="{ name: 'home' }" exact>
+          <q-item-section avatar>
+            <q-icon name="home" class="menu__icons--branded" />
+          </q-item-section>
+          <q-item-section class="text-dark">Home</q-item-section>
+        </q-item>
+        <q-item :to="{ name: 'profile' }" exact>
+          <q-item-section avatar>
+            <q-icon name="person" class="menu__icons--branded" />
+          </q-item-section>
+          <q-item-section class="text-dark">Profile</q-item-section>
+        </q-item>
+        <q-item :to="{ name: 'bookings' }" exact>
+          <q-item-section avatar>
+            <q-icon name="flight" class="menu__icons--branded" />
+          </q-item-section>
+          <q-item-section class="text-dark">My Bookings</q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <transition enter-active-class="animated fadeIn" appear>
+        <router-view />
+      </transition>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      leftDrawer: true,
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.menu__icons--branded
+  font-size: 43px !important
+</style>
