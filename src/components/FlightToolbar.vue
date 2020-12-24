83c1bc35103df7fc6c6f8bb626e929b620f8b223
heitorlessa <lessa@amazon.co.uk>
2020-12-24 10:03:41 +0100

    feat: add sort dropdown opts
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>

--- src/components/FlightToolbar.vue
+++ src/components/FlightToolbar.vue
@@ -87,17 +87,80 @@
               >
               </q-btn>
               <span> | </span>
-              <q-btn
+              <q-btn-dropdown
                 class="text-bold"
-                dense
                 flat
                 text-color="primary"
                 size="0.75rem"
                 label="Sort"
                 padding="0"
-                @click="filters = true"
+                auto-close
               >
-              </q-btn>
+                <q-list separator>
+                  <q-item
+                    clickable
+                    @click="sortResults(SortPreference.LowestPrice)"
+                    :active="sortSelection == SortPreference.LowestPrice"
+                    active-class="bg-cyan-2 text-dark"
+                  >
+                    <q-item-section>
+                      <q-item-label class="text-subtitle1"
+                        >Lowest price</q-item-label
+                      >
+                      <q-item-label caption
+                        >Lowest to highest ticket price</q-item-label
+                      >
+                    </q-item-section>
+                  </q-item>
+
+                  <q-item
+                    clickable
+                    @click="sortResults(SortPreference.EarliestDeparture)"
+                    :active="sortSelection == SortPreference.EarliestDeparture"
+                    active-class="bg-cyan-2 text-dark"
+                  >
+                    <q-item-section>
+                      <q-item-label class="text-subtitle1"
+                        >Earliest departure</q-item-label
+                      >
+                      <q-item-label caption
+                        >Earliest to latest departure time
+                      </q-item-label>
+                    </q-item-section>
+                  </q-item>
+
+                  <q-item
+                    clickable
+                    @click="sortResults(SortPreference.HighestPrice)"
+                    :active="sortSelection == SortPreference.HighestPrice"
+                    active-class="bg-cyan-2 text-dark"
+                  >
+                    <q-item-section>
+                      <q-item-label class="text-subtitle1"
+                        >Highest price</q-item-label
+                      >
+                      <q-item-label caption
+                        >Highest to lowest ticket price</q-item-label
+                      >
+                    </q-item-section>
+                  </q-item>
+                  <q-item
+                    clickable
+                    @click="sortResults(SortPreference.LatestDeparture)"
+                    :active="sortSelection == SortPreference.LatestDeparture"
+                    active-class="bg-cyan-2 text-dark"
+                  >
+                    <q-item-section>
+                      <q-item-label class="text-subtitle1"
+                        >Latest departure</q-item-label
+                      >
+                      <q-item-label caption
+                        >Latest to earliest departure time</q-item-label
+                      >
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+              </q-btn-dropdown>
               <q-dialog v-model="filters">
                 <q-card style="width: 300px" class="">
                   <q-card-section class="filter__toolbar q-pa-none">
@@ -219,6 +282,13 @@
 import { date } from 'quasar'
 import { mapGetters } from 'vuex'
 
+const SortPreference = Object.freeze({
+  LowestPrice: 'lowestPrice',
+  HighestPrice: 'highestprice',
+  EarliestDeparture: 'earliestdeparture',
+  LatestDeparture: 'latestdeparture'
+})
+
 export default {
   /**
    *
@@ -266,7 +336,9 @@ export default {
       arrivalFilter: {
         min: this.$store.getters['catalog/earliestArrival'],
         max: this.$store.getters['catalog/latestArrival']
-      }
+      },
+      sortSelection: '',
+      SortPreference
     }
   },
   computed: {
@@ -285,6 +357,37 @@ export default {
   methods: {
     formatTimeRange(time) {
       return date.formatDate(time, 'HH:mm')
+    },
+    /**
+     * Sort flight results by a given preference e.g. price, schedule
+     *
+     * @param {SortPreference} preference - Array of Flight Class to sort on
+     * @example
+     * // return flights sorted from least to most expensive
+     * sortResults('lowestPrice')
+     */
+    sortResults(preference) {
+      switch (preference) {
+        case preference.LowestPrice:
+          console.info('Sorting by lowest price')
+          break
+
+        case preference.HighestPrice:
+          console.info('Sorting by highest price')
+          break
+
+        case preference.EarliestDeparture:
+          console.info('Sorting by earliest departure')
+          break
+
+        case preference.LatestDeparture:
+          console.info('Sorting by latest departure')
+          break
+
+        default:
+          break
+      }
+      this.sortSelection = preference
     }
   },
   mounted() {
