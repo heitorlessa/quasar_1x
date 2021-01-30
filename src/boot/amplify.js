b249c00a5f4e12ab21071b7c7c4d046a08bd215a
heitorlessa <lessa@amazon.co.uk>
2021-01-30 19:08:45 +0100

    improv: use LOG_LEVEL for Amplify logger
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>

--- src/boot/amplify.js
+++ src/boot/amplify.js
@@ -2,6 +2,7 @@ import Amplify from 'aws-amplify'
 import awsconfig from '../../aws-exports'
 import '@aws-amplify/ui-vue'
 
+const logLevel = process.env.LOG_LEVEL || 'INFO'
 Amplify.configure(awsconfig)
 
-Amplify.Logger.LOG_LEVEL = 'DEBUG'
+Amplify.Logger.LOG_LEVEL = logLevel
